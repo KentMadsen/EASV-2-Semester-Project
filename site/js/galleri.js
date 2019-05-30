80236d2bf5b9355bf8b8737af84c3045964d9dc4
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-05-31 01:48:06 +0200

    a

---
+++ site/js/galleri.js
@@ -0,0 +1,50 @@
+var galleri = new Vue(
+    {
+      el:'#galleri',
+  
+      data:
+      {
+        listedGalleryItems:
+        [
+            {
+                name:'',
+                game:'cs:go',
+                url:'https://i.imgur.com/uPVaFaC.png'
+            },
+            {
+                name:'',
+                game:'cs:go',
+                url:'https://i.imgur.com/6Ejz5Ml.png'
+            },
+            {
+                name:'',
+                game:'cs:go',
+                url:'https://i.imgur.com/lqNTCLU.png'
+            },
+            {
+                name:'',
+                game:'cs:go',
+                url:'https://i.imgur.com/15RTEth.png'
+            },
+            {
+                name:'',
+                game:'cs:go',
+                url:'https://i.imgur.com/IxdE1Qb.png'
+            }
+        ]
+          
+        
+  
+      },
+  
+      watch:
+      {
+  
+      },
+  
+      methods:
+      {
+        
+      }
+    }
+  );
